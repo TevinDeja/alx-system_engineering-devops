9c3efd576fa71a118ba1dcbc2c357eefb2bf9f98
TevinDeja <teviowena@gmail.com>
2024-05-07 03:02:28 -0700

    Count it!

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,18 +3,22 @@
 function that queries the Reddit API
 returns the number of sububscribers for a given subreddit
 """
-
+import json
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """Read reddit API and return number subscribers"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
+    """Returns a list of titles of all hot posts on a given subreddit"""
+    username = 'mistidevs'
+    password = 'Misati@2004'
+    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
+    headers = {'user-agent': '/u/mistidevs API Python for Holberton School'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    client = requests.session()
+    client.headers = headers
+    r = client.get(url, allow_redirects=False)
+    if r.status_code == 200:
+        return (r.json()["data"]["subscribers"])
     else:
-        return 0
+        return(0)
