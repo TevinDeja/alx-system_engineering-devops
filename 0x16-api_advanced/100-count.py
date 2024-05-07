745298ae3da615eb82f049d8bcc66793018e47b0
TevinDeja <teviowena@gmail.com>
2024-05-07 02:40:55 -0700

    Count it!

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -5,11 +5,9 @@ function that queries the Reddit API
 import requests
 
 def count_words(subreddit, word_list, after='', word_dict={}):
-    """ A function that queries the Reddit API parses the title of
-    all hot articles, and prints a sorted count of given keywords
-    (case-insensitive, delimited by spaces.
-    Javascript should count as javascript, but java should not).
-    If no posts match or the subreddit is invalid, it prints nothing.
+    """ 
+    A function that queries the Reddit API 
+    prints a sorted count of given keywords
     """
 
     if not word_dict:
